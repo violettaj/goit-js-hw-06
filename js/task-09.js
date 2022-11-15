9305455146359de90d8f66188a31ce3d3f47420b
Wiola Janczura <wiola.janczura@wp.pl>
2022-11-16 00:27:22 +0100

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
+
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btn = document.querySelector("button")
+const randomColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor()
+}
+btn.addEventListener('click', randomColor);
+
+
